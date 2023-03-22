a781f438a9fbb5065cc7bbe5a8832cee1198b468
kaitlinfrani <kaitlinfrani@csu.fullerton.edu>
2023-03-21 22:51:36 -0700

    Tried js changes

---
+++ HW4.js
@@ -0,0 +1,34 @@
+function addItem() {
+    // Get form input
+    var name = document.getElementById("univname").value;
+    var location = document.getElementById("location").value;
+    var photo = document.getElementById("photo").value;
+    var description = document.getElementById("description").value;
+
+    // Make new univ element
+    var newUniv = document.createElement("div");
+
+    // Add univ element to list
+    document.getElementById("universityList").appendChild(newUniv);
+
+    // Reset form data
+    document.getElementById("univname").value;
+    document.getElementById("location").value;
+    document.getElementById("photo").value;
+    document.getElementById("description").value;
+}
+
+function removeItem() {
+    // Remove university element from list
+    document.getElementById("universityList").removeChild(button.parentNode);
+    
+    // Display default content if list is empty
+    /*if (document.getElementById("universityList").childElementCount == 0) {
+        document.getElementById("defaultContent").style.display = "block";
+    }*/
+
+    // Display default form
+    document.getElementById("form").style.display = "block";
+    document.getElementById("form").reset();
+}
+
