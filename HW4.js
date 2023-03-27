e727b90081244412c49c71fc76172376f4aea2b1
kaitlinfrani <kaitlinfrani@csu.fullerton.edu>
2023-03-27 15:37:12 -0700

    updated js format

--- HW4.js
+++ HW4.js
@@ -1,35 +1,81 @@
-/*function addItem() {
-    // Get form input
-    var name = document.getElementById("univname").value;
-    var location = document.getElementById("location").value;
-    var photo = document.getElementById("photo").value;
-    var description = document.getElementById("description").value;
+// Select the form and output list elements
+const form = document.querySelector("form");
+const outputList = document.querySelector("#univlist");
 
-    // Make new univ element
-    var newUniv = document.createElement("div");
+// Select the default list item and remove button
+const defaultListItem = document.querySelector("#univitem");
+const defaultRemoveButton = defaultListItem.querySelector(".remove_btn");
 
-    // Add univ element to list
-    document.getElementById("universityList").appendChild(newUniv);
+// Disable the remove button
+defaultRemoveButton.disabled = true;
 
-    // Reset form data
-    document.getElementById("univname").value;
-    document.getElementById("location").value;
-    document.getElementById("photo").value;
-    document.getElementById("description").value;
+// Function to add a new item to the list
+function addItemToList(univname, location, photo, description) {
+  // Create a new list item element
+  const listItem = document.createElement("li");
+
+  // Create a structure for the list item
+  const listItemHtml = `
+    <img src="${photo}" alt="${univname}">
+      <h3>${univname}</h3>
+      <p>${location}</p>
+      <p>${description}</p>
+      <button class="remove_btn">Remove</button>
+  `;
+
+  // Set the HTML of the new list item
+  listItem.innerHTML = listItemHtml;
+
+  // Remove default list item if adding a new list item
+  if (outputList.firstElementChild == defaultListItem) {
+    outputList.removeChild(defaultListItem);
+  }
+  // Add the new list item to the output list
+  outputList.appendChild(listItem);
+
+  // Enable the default remove button if it was disabled
+  if (defaultRemoveButton.disabled) {
+    defaultRemoveButton.disabled = false;
+  }
 }
 
-function removeItem() {
-    // Remove university element from list
-    document.getElementById("universityList").removeChild(button.parentNode);
-    
-    // Display default content if list is empty
-    /*if (document.getElementById("universityList").childElementCount == 0) {
-        document.getElementById("defaultContent").style.display = "block";
-    }*/
+// Function to remove an item from the list
+function removeItemFromList(listItem) {
+  // Remove the list item from the output list
+  outputList.removeChild(listItem);
 
-/*
-    // Display default form
-    document.getElementById("form").style.display = "block";
-    document.getElementById("form").reset();
+  // If there are no more list items, show the default list item and disable its remove button
+  if (outputList.children.length === 0) {
+    outputList.appendChild(defaultListItem);
+    defaultRemoveButton.disabled = true;
+  }
 }
-*/
+
+// Event listener to the form to handle form submissions
+form.addEventListener("submit_btn", function (event) {
+  // Prevent the default form submission behavior
+  event.preventDefault();
+
+  // Get the form input values
+  const univname = form.elements["univname"].value;
+  const location = form.elements["location"].value;
+  const photo = form.elements["photo"].value;
+  const description = form.elements["description"].value;
+
+  // Add a new item to the list with the form input values
+  addItemToList(univname, location, photo, description);
+
+  // Reset the form
+  form.reset();
+});
+
+// Add an event listener to the output list to handle remove button clicks
+outputList.addEventListener("click", function (event) {
+  // If the clicked element is a remove button, remove its list item
+  if (event.target.classList.contains("remove_btn")) {
+    const listItem = event.target.closest("li");
+    if (listItem !== defaultListItem) {
+      removeItemFromList(listItem);
+    }
+  }
+});
